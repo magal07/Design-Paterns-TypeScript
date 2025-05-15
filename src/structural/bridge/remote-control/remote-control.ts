87b698ffef9dffee105cae38783d9ba17b63e5e4
Marcelo Magalhaesm <celoxd01@gmail.com>
2025-05-15 01:17:22 -0300

    Bridge

---
+++ src/structural/bridge/remote-control/remote-control.ts
@@ -0,0 +1,12 @@
+import type { DeviceImplementation } from '../device/device-implementation';
+
+export class RemoteControl {
+  constructor(protected device: DeviceImplementation) {}
+
+  togglePower(): void {
+    this.device.setPower(!this.device.getPower());
+    console.log(
+      `${this.device.getName()} power status: ${this.device.getPower()}`,
+    );
+  }
+}
